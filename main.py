e2a92e8e4a462555acd8b0007090fd541f1d7bc9
evgeny426 <bardygin1@yandex.ru>
2024-09-09 21:29:30 +0400

    new code 1

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
 
-print('Я конфликтую')
-
-for i in range(8):
-    print(i)
-a, b = 9, 8
-if a > b:
-    print('a больше чем b')
-elif b > a:
-    print('b больше чем a')
-else:
-    print('a равно b')
-
+a = 5
